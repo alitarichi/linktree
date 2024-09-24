fbc694891fff83dde98bca18541a95e82dd9f2bf
ali tarichi <tarichiali@gmail.com>
2024-09-24 15:00:13 +0300

    removed some stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import data from "../data.json";
+import Image from "next/image";
+
 export default function Home() {
-  return <>Hello world!</>;
+  return (
+    <div className="flex justify-center mx-auto w-full mt-16">
+      <Image
+        alt={data.name}
+        src={data.avatar}
+        width={80}
+        height={80}
+        className="rounded-full"
+      />
+      <h1>{data.name}</h1>
+    </div>
+  );
 }
